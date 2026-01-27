5ba00b40592ff800fa3117efe3c85b6afb6da562
qqweweqewe <golubkovivan4@gmail.com>
2026-01-27 16:15:08 +0300

    separated messages into jsons
    so no need to recompile if changes needed!

--- src/messages.rs
+++ src/messages.rs
@@ -2,31 +2,13 @@ use rand::Rng;
 use std::collections::HashMap;
 use std::fs;
 
-const JOIN_MESSAGES: &[&str] = &[
-    r"⟐ *{player}* бля опять ты",
-    r"⟐ *{player}* на месте🫡",
-    r"⟐ привет *{player}*\!",
-    r"⟐ *{player}* залетает на серв",
-    r"⟐ лютый *{player}* жи есть",
-    r"⟐ *{player}* снова тут\!",
-    r"⟐ *{player}* просто взял вошел",
-    r"⟐ кто составит компанию *{player}*?",
-    r"⟐ *{player}* материализовался",
-    r"⟐ *{player}* присоединяется",
-    r"⟐ *{player}* пожаловал",
-    r"⟐ ого какие люди и без охраны привет *{player}*"
-];
+fn load_messages(file_path: &str) -> Vec<String> {
+    fs::read_to_string(file_path)
+        .ok()
+        .and_then(|content| serde_json::from_str(&content).ok())
+        .unwrap_or_default()
+}
 
-const LEAVE_MESSAGES: &[&str] = &[
-    r"⁛ *{player}* ушел в закат",
-    r"⁛ *{player}* мама позвала кушать",
-    r"⁛ *{player}* наконец съебал",
-    r"⁛ *{player}* до связи\!",
-    r"⁛ *{player}* ну куда собрался нууу вернись",
-    r"⁛ Adios, *{player}*\!",
-];
-
-// IMPORTANT PART
 fn load_special_messages(file_path: &str) -> HashMap<String, Vec<String>> {
     fs::read_to_string(file_path)
         .ok()
@@ -49,7 +31,8 @@ pub fn get_random_join_message(player_name: &str) -> String {
     let template = if let Some(messages) = special_messages.get(player_name) {
         &messages[rng.gen_range(0..messages.len())]
     } else {
-        JOIN_MESSAGES[rng.gen_range(0..JOIN_MESSAGES.len())]
+        let regular_messages = load_messages("join_messages.json");
+        &regular_messages[rng.gen_range(0..regular_messages.len())]
     };
     template.replace("{player}", &escape_markdown(player_name))
 }
@@ -60,7 +43,8 @@ pub fn get_random_leave_message(player_name: &str) -> String {
     let template = if let Some(messages) = special_messages.get(player_name) {
         &messages[rng.gen_range(0..messages.len())]
     } else {
-        LEAVE_MESSAGES[rng.gen_range(0..LEAVE_MESSAGES.len())]
+        let regular_messages = load_messages("leave_messages.json");
+        &regular_messages[rng.gen_range(0..regular_messages.len())]
     };
     template.replace("{player}", &escape_markdown(player_name))
 }
